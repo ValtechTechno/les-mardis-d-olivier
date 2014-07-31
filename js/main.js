7064dbc521044e53a4ff03b94ab50268ba2d64b2
hiks <kitnhiks@gmail.com>
2014-07-31 18:30:09 +0200

    I can create a distribution with a given date

---
+++ js/main.js
@@ -0,0 +1,19 @@
+(function() {
+    var app = angular.module('mardisDolivier', []);
+
+    app.controller('contentCtrl', function($scope){
+        Date.prototype.yyyymmdd = function() {
+            var yyyy = this.getFullYear().toString();
+            var mm = (this.getMonth()+1).toString(); // getMonth() is zero-based
+            var dd  = this.getDate().toString();
+            return yyyy + (mm[1]?mm:"0"+mm[0]) + (dd[1]?dd:"0"+dd[0]); // padding
+        };
+
+        var today = new Date();
+        var day = today.getDate().toString();
+        var month = (today.getMonth()+1).toString();
+        var year = today.getFullYear().toString();
+        $scope.distributionDate = (day.length==1?"0"+day:day)+"/"+(month.length==1?"0"+month:month)+"/"+year;
+    });
+
+})();
