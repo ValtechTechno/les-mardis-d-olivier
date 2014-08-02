82b0c2b1d08b50457eff0f8dffbaec4e932359b6
Éric Le Merdy <eric.lemerdy@laposte.net>
2014-08-03 00:28:36 +0200

    Remove unused format method

--- js/main.js
+++ js/main.js
@@ -9,13 +9,7 @@
         $scope.distributionDate = (day.length==1?"0"+day:day)+"/"+(month.length==1?"0"+month:month)+"/"+year;
     });
 
-    app.factory('Date',function(){
-        Date.prototype.yyyymmdd = function() {
-            var yyyy = this.getFullYear().toString();
-            var mm = (this.getMonth()+1).toString(); // getMonth() is zero-based
-            var dd  = this.getDate().toString();
-            return yyyy + (mm[1]?mm:"0"+mm[0]) + (dd[1]?dd:"0"+dd[0]); // padding
-        };
+    app.factory('Date',function() {
         return new Date();
     });
 
