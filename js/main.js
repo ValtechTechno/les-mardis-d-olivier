18e3cd85ecd2126d473fee978b693889214a5537
Éric Le Merdy <eric.lemerdy@laposte.net>
2014-08-03 04:20:50 +0200

    Formattage du code.

--- js/main.js
+++ js/main.js
@@ -1,94 +1,94 @@
 (function() {
-    if(typeof localStorage=='undefined') {
-        alert("localStorage n'est pas supporté, l'application ne fonctionnera pas avec ce navigateur.");
+  if (typeof localStorage == 'undefined') {
+    alert("localStorage n'est pas supporté, l'application ne fonctionnera pas avec ce navigateur.");
+  }
+
+  var app = angular.module('mardisDolivier', []);
+
+  app.controller('contentCtrl', function($scope, $filter, Date) {
+    var today = Date;
+    $scope.distributionDate = $filter('date')(today, 'dd/MM/yyyy');
+
+    $scope.addBeneficiaire = function(firstName, lastName) {
+      if ($scope.beneficiaires.filter(function (beneficiaire) {
+        return beneficiaire.firstName === firstName && beneficiaire.lastName === lastName;
+      }).length > 0) {
+        return;
+      }
+      if (firstName === undefined || firstName.length == 0) {
+        return;
+      }
+      if (lastName === undefined || lastName.length == 0) {
+        return;
+      }
+      $scope.beneficiaires.push({firstName:firstName, lastName:lastName});
+    };
+
+    $scope.beneficiaires = angular.fromJson(localStorage.getItem('beneficiaires'));
+    if ($scope.beneficiaires === null) {
+      $scope.beneficiaires = [];
     }
 
-    var app = angular.module('mardisDolivier', []);
+    $scope.$watch('beneficiaires', function(newValue, oldValue) {
+      localStorage.setItem('beneficiaires', angular.toJson($scope.beneficiaires));
+    }, true);
 
-    app.controller('contentCtrl', function($scope, $filter, Date) {
-        var today = Date;
-        $scope.distributionDate = $filter('date')(today, 'dd/MM/yyyy');
+    $scope.showAllDistribution = function() {
+      $scope.distributions = retrieveAllDistribution();
+    };
+    $scope.showAllDistribution();
 
-        $scope.addBeneficiaire = function(firstName, lastName) {
-            if ($scope.beneficiaires.filter(function (beneficiaire) {
-                return beneficiaire.firstName === firstName && beneficiaire.lastName === lastName;
-            }).length > 0) {
-                return;
-            }
-            if (firstName === undefined || firstName.length == 0) {
-                return;
-            }
-            if (lastName === undefined || lastName.length == 0) {
-                return;
-            }
-            $scope.beneficiaires.push({firstName:firstName, lastName:lastName});
-        };
+    $scope.startNewDistribution = function() {
+      try {
+        $scope.saveNewDistribution();
+      }catch(err){
+        alert(err);
+        return;
+      }
+      $scope.showAllDistribution();
+      $scope.distributionStarted = true;
+    };
 
-        $scope.beneficiaires = angular.fromJson(localStorage.getItem('beneficiaires'));
-        if ($scope.beneficiaires === null) {
-            $scope.beneficiaires = [];
-        }
+    $scope.saveNewDistribution = function() {
+      storeDistribution({
+        "date":$scope.distributionDate,
+        "nbPlannedMeals":$scope.distributionNbPlannedMeals
+      });
+    };
+  });
 
-        $scope.$watch('beneficiaires', function(newValue, oldValue) {
-            localStorage.setItem('beneficiaires', angular.toJson($scope.beneficiaires));
-        }, true);
-
-        $scope.showAllDistribution = function(){
-            $scope.distributions = retrieveAllDistribution();
-        };
-        $scope.showAllDistribution();
-
-        $scope.startNewDistribution = function(){
-            try {
-                $scope.saveNewDistribution();
-            }catch(err){
-                alert(err);
-                return;
-            }
-            $scope.showAllDistribution();
-            $scope.distributionStarted = true;
-        };
-
-        $scope.saveNewDistribution = function(){
-            storeDistribution({
-                "date":$scope.distributionDate,
-                "nbPlannedMeals":$scope.distributionNbPlannedMeals
-            });
-        };
-    });
-
-    app.factory('Date',function() {
-        return new Date();
-    });
+  app.factory('Date',function() {
+    return new Date();
+  });
 
 })();
 
 retrieveAllDistribution = function(){
-    var allDistributions = angular.fromJson(localStorage.getItem('distributions'));
-    if (allDistributions == null){
-        allDistributions = [];
-    }else{
-        allDistributions.reverse();
-    }
-    return allDistributions;
+  var allDistributions = angular.fromJson(localStorage.getItem('distributions'));
+  if (allDistributions == null) {
+    allDistributions = [];
+  } else {
+    allDistributions.reverse();
+  }
+  return allDistributions;
 }
 
 storeDistribution = function(distribution){
-    if (distribution.date === undefined || distribution.date.length == 0) {
-        throw "merci de renseigner la date";
-    }
-    if (distribution.nbPlannedMeals === undefined || distribution.nbPlannedMeals.length == 0) {
-        throw "merci de renseigner le nombre de repas";
-    }
-    var distributions = angular.fromJson(localStorage.getItem('distributions'));
-    if (distributions==null){
-        distributions = [];
-    }else if (distributions.filter(function (storedDistribution) {
-        return distribution.date === storedDistribution.date;
-    }).length > 0) {
-        throw "Une distribution a cette date existe déjà";
-    }
+  if (distribution.date === undefined || distribution.date.length == 0) {
+    throw "merci de renseigner la date";
+  }
+  if (distribution.nbPlannedMeals === undefined || distribution.nbPlannedMeals.length == 0) {
+    throw "merci de renseigner le nombre de repas";
+  }
+  var distributions = angular.fromJson(localStorage.getItem('distributions'));
+  if (distributions == null) {
+    distributions = [];
+  } else if (distributions.filter(function (storedDistribution) {
+    return distribution.date === storedDistribution.date;
+  }).length > 0) {
+    throw "Une distribution a cette date existe déjà";
+  }
 
-    distributions.push(distribution);
-    localStorage.setItem('distributions',angular.toJson(distributions));
+  distributions.push(distribution);
+  localStorage.setItem('distributions',angular.toJson(distributions));
 }
--- tests/HelloSpec.js
+++ tests/HelloSpec.js
@@ -1,78 +1,78 @@
 describe('Initialisation check', function() {
 
-    var scope;
+  var scope;
 
-    beforeEach(angular.mock.module('mardisDolivier'));
-    beforeEach(function() {
-        localStorage.clear()
+  beforeEach(angular.mock.module('mardisDolivier'));
+  beforeEach(function() {
+    localStorage.clear()
+  });
+  beforeEach(angular.mock.inject(function($rootScope, $controller, $filter){
+    scope = $rootScope.$new();
+    $controller('contentCtrl', {
+      $scope: scope,
+      $filter: $filter,
+      Date: new Date(1981, 11, 24)
     });
-    beforeEach(angular.mock.inject(function($rootScope, $controller, $filter){
-        scope = $rootScope.$new();
-        $controller('contentCtrl', {
-            $scope: scope,
-            $filter: $filter,
-            Date: new Date(1981, 11, 24)
-        });
-    }));
+  }));
 
-    it('should have the date defined', function () {
-        expect(scope.distributionDate).toBe('24/12/1981');
-    });
+  it('should have the date defined', function () {
+    expect(scope.distributionDate).toBe('24/12/1981');
+  });
 
-    it('should add a beneficiaire', function () {
-        scope.addBeneficiaire('John', 'Rambo');
+  it('should add a beneficiaire', function () {
+    scope.addBeneficiaire('John', 'Rambo');
 
-        expect(scope.beneficiaires).toContain({firstName:'John',lastName:'Rambo'});
-    });
+    expect(scope.beneficiaires).toContain({firstName:'John',lastName:'Rambo'});
+  });
 
-    it('should not allow to add an existing beneficiaire', function () {
-        scope.beneficiaires.push({firstName:'John',lastName:'Rambo'});
+  it('should not allow to add an existing beneficiaire', function () {
+    scope.beneficiaires.push({firstName:'John',lastName:'Rambo'});
 
-        scope.addBeneficiaire('John', 'Rambo');
+    scope.addBeneficiaire('John', 'Rambo');
 
-        expect(scope.beneficiaires.length).toBe(1);
-    });
+    expect(scope.beneficiaires.length).toBe(1);
+  });
 
-    it('should not allow to add a beneficiaire with empty first name or last name', function () {
-        scope.addBeneficiaire('', '');
-        scope.addBeneficiaire('John', '');
-        scope.addBeneficiaire('', 'Rambo');
+  it('should not allow to add a beneficiaire with empty first name or last name', function () {
+    scope.addBeneficiaire('', '');
+    scope.addBeneficiaire('John', '');
+    scope.addBeneficiaire('', 'Rambo');
 
-        expect(scope.beneficiaires.length).toBe(0);
-    });
+    expect(scope.beneficiaires.length).toBe(0);
+  });
 
-    it('should save beneficiaires to localStorage', function () {
-        scope.addBeneficiaire('foo', 'bar');
-        scope.$digest();
+  it('should save beneficiaires to localStorage', function () {
+    scope.addBeneficiaire('foo', 'bar');
+    scope.$digest();
 
-        expect(localStorage.getItem('beneficiaires')).toBe('[{"firstName":"foo","lastName":"bar"}]');
-    });
+    expect(localStorage.getItem('beneficiaires')).toBe('[{"firstName":"foo","lastName":"bar"}]');
+  });
 
-    it("is is possible to save a new distribution", function () {
-        scope.distributionNbPlannedMeals = "50";
-        scope.distributionDate = "12/12/2012"
-        scope.saveNewDistribution();
-        expect(retrieveAllDistribution()).toEqual([{"date":"12/12/2012", "nbPlannedMeals":"50"}]);
-    });
+  it("is is possible to save a new distribution", function () {
+    scope.distributionNbPlannedMeals = "50";
+    scope.distributionDate = "12/12/2012"
+    scope.saveNewDistribution();
+    expect(retrieveAllDistribution()).toEqual([{"date":"12/12/2012", "nbPlannedMeals":"50"}]);
+  });
 
-    it("shouldn't be possible to save two distribution at the same date", function () {
-        scope.distributionNbPlannedMeals = "50";
-        scope.distributionDate = "12/12/2012"
-        scope.saveNewDistribution();
-        try{scope.saveNewDistribution();}catch(err){}
-        expect(retrieveAllDistribution()).toEqual([{"date":"12/12/2012", "nbPlannedMeals":"50"}]);
-    })
+  it("shouldn't be possible to save two distribution at the same date", function () {
+    scope.distributionNbPlannedMeals = "50";
+    scope.distributionDate = "12/12/2012"
+    scope.saveNewDistribution();
+    try{scope.saveNewDistribution();}catch(err){}
+    expect(retrieveAllDistribution()).toEqual([{"date":"12/12/2012", "nbPlannedMeals":"50"}]);
+  })
 
-    it('should not allow to add a distribution with empty date or number of meals', function () {
-        scope.distributionNbPlannedMeals = "";
-        scope.distributionDate = ""
-        try{scope.saveNewDistribution();}catch(err){}
-        scope.distributionNbPlannedMeals = "50";
-        scope.distributionDate = ""
-        try{scope.saveNewDistribution();}catch(err){}
-        scope.distributionNbPlannedMeals = "";
-        scope.distributionDate = "12/12/2012"
-        try{scope.saveNewDistribution();}catch(err){}
-        expect(retrieveAllDistribution()).toEqual([]);
-    });
+  it('should not allow to add a distribution with empty date or number of meals', function () {
+    scope.distributionNbPlannedMeals = "";
+    scope.distributionDate = ""
+    try{scope.saveNewDistribution();}catch(err){}
+    scope.distributionNbPlannedMeals = "50";
+    scope.distributionDate = ""
+    try{scope.saveNewDistribution();}catch(err){}
+    scope.distributionNbPlannedMeals = "";
+    scope.distributionDate = "12/12/2012"
+    try{scope.saveNewDistribution();}catch(err){}
+    expect(retrieveAllDistribution()).toEqual([]);
+  });
 });
