59cafc08a39d4335d8f0920714f0ce797fbb3f83
Éric Le Merdy <eric.lemerdy@laposte.net>
2015-03-06 00:36:22 +0100

    refactor(popup): use angular instead of jquery and move to another dir.

--- app/widget/popup/popupDirective.js
+++ app/widget/popup/popupDirective.js
@@ -12,17 +12,17 @@
           popupLabel: '@',
           popupButton: '=',
           event: '@',
-          style: "@"
+          style: '@'
         },
         link: function (scope, element, attrs) {
           scope.popupDirectiveId = 'popupDirective' + id++;
           element.find('div').attr('id', scope.popupDirectiveId);
           scope.openPopup = function () {
-            $('#' + scope.popupDirectiveId).foundation('reveal', 'open');
+            angular.element('#' + scope.popupDirectiveId).foundation('reveal', 'open');
           };
           scope.action = function () {
             if (this.button.close === true) {
-              $('#' + scope.popupDirectiveId).foundation('reveal', 'close');
+              angular.element('#' + scope.popupDirectiveId).foundation('reveal', 'close');
             }
             if (this.button.event) {
               scope.$emit(this.button.event);
