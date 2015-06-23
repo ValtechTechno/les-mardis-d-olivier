a8f2e39298dbf1fbe4d82211a3973aeae4a4b5ba
kelvinieg <chauvignef@gmail.com>
2015-06-24 00:25:07 +0200

    feat(login) : refactor menu displaying

--- app/menu.controller.js
+++ app/menu.controller.js
@@ -5,14 +5,30 @@
     .module('mardisDolivier')
     .controller('MenuController', MenuController);
 
-  function MenuController($location) {
+  function MenuController($location, LoginService, $rootScope) {
     var vm = this;
     vm.isActive = isActive;
+    vm.logout = logout;
+    vm.isAuth = isAuth;
+    vm.isUserNotAuth = isUserNotAuth;
     function isActive(path) {
       if ($location.path().substr(0, path.length) === path) {
-        return "active";
+        return true;
       }
-      return "";
+      return false;
+    }
+
+    function logout(){
+      LoginService.logout();
+      $location.path("/login");
+    }
+
+    function isAuth(){
+      return !vm.isUserNotAuth();
+    }
+
+    function isUserNotAuth(){
+      return $rootScope.account === null || $rootScope.account === undefined;
     }
 
   }
